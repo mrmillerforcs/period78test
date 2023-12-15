0b0f5412b1667218d075c6da51ced615faa9ae4f
laptop <sample@bths.edu>
2023-12-15 13:07:59 -0500

    add print

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("object Object");
+        System.out.println("period 7-8!");
     }
 }
