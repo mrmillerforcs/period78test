c4d882d1cca516c18463e0d7fff06f9185d1f314
Sample <sample@bths.edu>
2023-12-15 13:19:43 -0500

    overrode Mr. Miller's statement

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello!");
         System.out.println("object Object");
-        System.out.println("period 7-8!");
+        System.out.println("period 7-8!sdkfjklsdfjklwdfjklsdfjklsdfjlfjsdlf");
         System.out.println("whoa thats crazy");
         System.out.println("Don't worry about AI taking over CS. People will drop like flies and you'll be able to snag a job at 5x the salary.");
         // :OOOO
